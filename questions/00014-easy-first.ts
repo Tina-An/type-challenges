3e67f4761b3fa05c0a1748f5ad9996e2c4772d56
tinadong <tinadong@futunn.com>
2024-09-30 15:46:23 +0800

    feat: add solution for 14th and 15th question

--- questions/00014-easy-first.ts
+++ questions/00014-easy-first.ts
@@ -22,7 +22,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type First<T extends any[]> = any
+type First<T extends any[]> = T['length'] extends 0 ? never :T[0];
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
