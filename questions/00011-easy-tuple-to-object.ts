446a0cd4a47fec92a5c9d7675e28684adf13ef0a
tinadong <tinadong@futunn.com>
2024-09-30 15:41:53 +0800

    feat: add solution for 11th question

--- questions/00011-easy-tuple-to-object.ts
+++ questions/00011-easy-tuple-to-object.ts
@@ -19,8 +19,12 @@
 */
 
 /* _____________ 你的代码 _____________ */
+// type T = readonly any[];
+// type t = T[keyof T];
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P;
+}
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
