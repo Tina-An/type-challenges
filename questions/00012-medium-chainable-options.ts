b17d359840178333f4bc431f227f58dfdfcfecdd
tinadong <tinadong@futunn.com>
2024-11-15 17:16:55 +0800

    feat: add 12th medium question

--- questions/00012-medium-chainable-options.ts
+++ questions/00012-medium-chainable-options.ts
@@ -38,10 +38,16 @@
 */
 
 /* _____________ 你的代码 _____________ */
+type Omit<T, K> = {
+  [P in keyof T as P extends K ? never : P]: T[P] 
+}
 
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T = {}> = {
+  option<K extends string, V>(key: 
+    K extends keyof T ? 
+    never
+    : K, value: V): Chainable<Omit<T, K> & Record<K, V>>
+  get(): T
 }
 
 /* _____________ 测试用例 _____________ */
