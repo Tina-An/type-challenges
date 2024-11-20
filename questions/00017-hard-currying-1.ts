64df4269c1146d834bd1977c65f1bc8a6de8fb41
tinadong <tinadong@futunn.com>
2024-11-20 11:23:15 +0800

    feat: add 17th question

--- questions/00017-hard-currying-1.ts
+++ questions/00017-hard-currying-1.ts
@@ -27,9 +27,46 @@
 */
 
 /* _____________ 你的代码 _____________ */
+// 函数入参的类型推断
+declare function getValue<T>(x: T): T
+const v1 = getValue('123'); // '123'
+// same as
+const v11 = getValue('123' as const);
+type cases1 = [
+  Expect<Equal<typeof v1, typeof v11>>,
+]
+const v2 = getValue(123); // 123
+type cases2 = [
+  Expect<Equal<typeof v1, '123'>>,
+  Expect<Equal<typeof v2, 123>>,
+]
 
-declare function Currying(fn: any): any
+// 关于const常量的类型推断
+const v12 = 'test';
+let v13 = 'test';
+const v14 = { a: 123 };
+type t11 = typeof v11; // '123'
+type t12 = typeof v12; // 'test'
+type t13 = typeof v13; // string
+type t14 = typeof v14; // { a: number }
 
+// answer1
+// type CurryingReturn<T extends Function> = T extends (a: infer A) => infer R ? T : 
+//   T extends (...args: [infer F, ...infer L]) => infer R ? (arg: F) => CurryingReturn<(...args: L) => R> : T
+
+// answer2
+type FirstAsTuple<T> = T extends [any, ...infer R]
+  ? T extends [...infer F, ...R]
+    ? F
+    : never
+  : never
+type CurryingReturn<F> = F extends (...args: infer Args) => infer Return 
+  ? Args['length'] extends 0 | 1
+    ? F
+    : Args extends [any, ...infer Rest] ? (...arg: FirstAsTuple<Args>) => CurryingReturn<(...args: Rest) => Return> : never
+  : never
+
+declare function Currying<T extends Function>(fn: T): CurryingReturn<T>
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
