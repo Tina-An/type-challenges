06edf4c04afabfe10444a4b43838f37ae8a79d80
Tina-An <63850850+Tina-An@users.noreply.github.com>
2025-04-19 12:09:05 +0800

    Update 00151-extreme-query-string-parser.ts

--- questions/00151-extreme-query-string-parser.ts
+++ questions/00151-extreme-query-string-parser.ts
@@ -1,1 +1,78 @@
+/*
+  151 - Query String Parser
+  -------
+  by Pig Fang (@g-plane) #extreme #template-literal
 
+  ### Question
+
+  You're required to implement a type-level parser to parse URL query string into a object literal type.
+
+  Some detailed requirements:
+
+  - Value of a key in query string can be ignored but still be parsed to `true`. For example, `'key'` is without value, so the parser result is `{ key: true }`.
+  - Duplicated keys must be merged into one. If there are different values with the same key, values must be merged into a tuple type.
+  - When a key has only one value, that value can't be wrapped into a tuple type.
+  - If values with the same key appear more than once, it must be treated as once. For example, `key=value&key=value` must be treated as `key=value` only.
+
+  > View on GitHub: https://tsch.js.org/151
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type ParseSingleString<T extends string> = T extends `${infer K}=${infer V}`
+  ? [K, V]
+  : [T, true]
+
+type GetRes<T extends [string, string | true], Res = {}> = T[0] extends keyof Res
+  ? {
+    [K in keyof Res]: K extends T[0]
+      ? Res[K] extends any[]
+        ? T[1] extends Res[K][number]
+          ? Res[K]
+          : [...Res[K], T[1]]
+        : T[1] extends Res[K]
+          ? Res[K]
+          : [Res[K], T[1]]
+      : Res[K]
+  }
+  : {
+    [K in T[0] | keyof Res]: K extends keyof Res
+      ? Res[K]
+      : T[1]
+  }
+
+type ParseQueryString<T extends string, Res = {}> = T extends '' 
+  ? Res
+  : T extends `${infer Item}&${infer Last}`
+    ? ParseQueryString<Last, GetRes<ParseSingleString<Item>, Res>>
+    : GetRes<ParseSingleString<T>, Res>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type t1 = ParseQueryString<'k1=v1&k1=v2'>
+
+type cases = [
+  Expect<Equal<ParseQueryString<''>, {}>>,
+  Expect<Equal<ParseQueryString<'k1'>, { k1: true }>>,
+  Expect<Equal<ParseQueryString<'k1&k1'>, { k1: true }>>,
+  Expect<Equal<ParseQueryString<'k1&k2'>, { k1: true, k2: true }>>,
+  Expect<Equal<ParseQueryString<'k1=v1'>, { k1: 'v1' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k1=v2'>, { k1: ['v1', 'v2'] }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2=v2'>, { k1: 'v1', k2: 'v2' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2=v2&k1=v2'>, { k1: ['v1', 'v2'], k2: 'v2' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2'>, { k1: 'v1', k2: true }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k1=v1'>, { k1: 'v1' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k1=v2&k1=v1'>, { k1: ['v1', 'v2'] }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2=v1&k1=v2&k1=v1'>, { k1: ['v1', 'v2'], k2: 'v1' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k2=v2&k1=v2&k1=v3'>, { k1: ['v1', 'v2', 'v3'], k2: 'v2' }>>,
+  Expect<Equal<ParseQueryString<'k1=v1&k1'>, { k1: ['v1', true] }>>,
+  Expect<Equal<ParseQueryString<'k1&k1=v1'>, { k1: [true, 'v1'] }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/151/answer
+  > View solutions: https://tsch.js.org/151/solutions
+  > More Challenges: https://tsch.js.org
+*/
