7beb3e1816cc98bdde133eaa1680c41ad7536526
Tina-An <63850850+Tina-An@users.noreply.github.com>
2025-05-10 15:02:41 +0800

    Update 00189-easy-awaited.ts

--- questions/00189-easy-awaited.ts
+++ questions/00189-easy-awaited.ts
@@ -22,7 +22,11 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyAwaited<T> = any
+type MyAwaited<T extends { then: (args: (args: any) => any) => any }> = T extends { then: (args: (args: infer R) => any) => any }
+  ? R extends { then: (args: (args: any) => any) => any }
+    ? MyAwaited<R>
+    : R
+  : never
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
