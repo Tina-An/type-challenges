07aaaedcc548c3a27fae14e3a552c30de56b95b4
Tina-An <63850850+Tina-An@users.noreply.github.com>
2025-05-24 11:41:48 +0800

    Update 09616-medium-parse-url-params.ts

--- questions/09616-medium-parse-url-params.ts
+++ questions/09616-medium-parse-url-params.ts
@@ -1,1 +1,47 @@
+/*
+  9616 - Parse URL Params
+  -------
+  by Anderson. J (@andersonjoseph) #medium #infer #string #template-literal
+
+  ### Question
+
+  You're required to implement a type-level parser to parse URL params string into an Union.
+
+  ```ts
+  ParseUrlParams<':id'> // id
+  ParseUrlParams<'posts/:id'> // id
+  ParseUrlParams<'posts/:id/:user'> // id | user
+  ```
+
+  > View on GitHub: https://tsch.js.org/9616
+*/
+
+/* _____________ Your Code Here _____________ */
+type GetParam<T extends string> = T extends `:${infer Param}`
+  ? Param
+  : never
+
+type ParseUrlParams<T extends string> = 
+  T extends `${infer First}/${infer Rest}`
+    ? GetParam<First> | ParseUrlParams<Rest>
+    : GetParam<T>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<ParseUrlParams<''>, never>>,
+  Expect<Equal<ParseUrlParams<':id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user'>, 'id' | 'user'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user/like'>, 'id' | 'user'>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/9616/answer
+  > View solutions: https://tsch.js.org/9616/solutions
+  > More Challenges: https://tsch.js.org
+*/
 
