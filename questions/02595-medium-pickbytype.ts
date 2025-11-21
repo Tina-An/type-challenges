9cb6c74d9354f6e969e6e1ebdb5db6d3fb734132
Tina-An <63850850+Tina-An@users.noreply.github.com>
2025-11-21 17:34:41 +0800

    Create 02595-medium-pickbytype.ts

---
+++ questions/02595-medium-pickbytype.ts
@@ -0,0 +1,66 @@
+/*
+  2595 - PickByType
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+
+  ### Question
+
+  From `T`, pick a set of properties whose type are assignable to `U`.
+
+  For Example
+
+  ```typescript
+  type OnlyBoolean = PickByType<{
+    name: string
+    count: number
+    isReadonly: boolean
+    isEnable: boolean
+  }, boolean> // { isReadonly: boolean; isEnable: boolean; }
+  ```
+
+  > View on GitHub: https://tsch.js.org/2595
+*/
+
+/* _____________ Your Code Here _____________ */
+type a1 = never extends number ? '1' : '0'; // '1'
+type a2 = never extends never ? '1': '0'
+type a3 = string extends never ? 1 : 0 // 0
+
+type PickByType<T, U> = { 
+  [K in keyof T as 
+    [U] extends [T[K]]
+      ? [T[K]] extends [U] ? K : never
+      : never]
+  :T[K]
+}
+
+// {
+//   [K in keyof T as T[K] extends U ? ([T[K]] extends [never] ? never : K) : never]: T[K];
+// };
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Model {
+  name: string
+  count: number
+  isReadonly: boolean
+  isEnable: boolean
+  isNerver: never
+}
+
+type t1 = PickByType<Model, string> // { name: string; }
+type t2 = PickByType<Model, never> // { isNerver: never; }
+
+type cases = [
+  Expect<Equal<PickByType<Model, boolean>, { isReadonly: boolean, isEnable: boolean }>>,
+  Expect<Equal<PickByType<Model, string>, { name: string }>>,
+  Expect<Equal<PickByType<Model, number>, { count: number }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2595/answer
+  > View solutions: https://tsch.js.org/2595/solutions
+  > More Challenges: https://tsch.js.org
+*/
